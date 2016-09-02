ce180722f6840a6a846a5684f34a6fe8b6b69e3c
jacob135135 <jb1311@york.ac.uk>
2016-09-02 21:05:02 +0200

    Moved some methods to dedicated MyUtils class Moved data_entry_option_prompt(); Renamed get_decimal_digit(...) to binary_nibble_to_decimal_digit(...) and moved Moved parse_CSV(...); Moved prepend_zeroes_if_needed(...); Moved request_file(); Moved request_input();

--- Log2SubBand/src/log2subband/Log2SubBand.java
+++ Log2SubBand/src/log2subband/Log2SubBand.java
@@ -1,23 +1,15 @@
 /*
  * Based on table on page 112.
- * Algorithm should (hopefully) be semantically same (work the same) as one on pages 141 & 142
+ * Algorithm should (hopefully) be semantically same (work the same) as one on pages 141 & 142 in thesis
  *
  * Current implementation assumptions:
- *  Range of numbers "allowed" is from 0 to 999 (inclusive)
- *  Result will have 12 bits -> [4,4,4]
+ * Range of numbers "allowed" is from 0 to 999 (inclusive)
+ * Result will have 12 bits -> [4,4,4]
  */
 package log2subband;
 
-import com.opencsv.CSVReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane; // Needed for input box at the beginning
+import static log2subband.MyUtils.decimal_to_binary;
+import static log2subband.MyUtils.get_decimal_digit;
 
 /**
  * @author Jakub
@@ -43,7 +35,7 @@ public class Log2SubBand {
      * @param input_number String 
      * @return String compressed binary number as string (e.g. "11000100010001") this is to simplify operations on the "number"
      */
-    public static String get_compressed_data(String input_number) {    
+    public static String compressed_data(String input_number) {    
         char digit_pos2 = input_number.charAt(2);
         char digit_pos1 = input_number.charAt(1);
         char digit_pos0 = input_number.charAt(0);
@@ -64,53 +56,7 @@ public class Log2SubBand {
         previous_digit_pos2 = digit_pos2;
         return return_value;
     }
-        
-    /**
-     * Prompts the user to enter an input.
-     * The input numbers are then separated by comma and extra spaces are removed
-     * @return raw_values Array of integers given by user
-     */
-    public static String[] request_input() {
-        String answer = JOptionPane.showInputDialog(null, "Type in numbers separated by comma.");
-        String[] raw_values = answer.split(",[ ]*"); // Separates numbers by comma, removes extra spaces
-        if (debug) System.out.println(answer);
-        return raw_values;
-    }
-    
-    /**
-     * Converts number in character representation(e.g. '1') into binary String representation (e.g. "0001") 
-     * COnverted number has 4 digits and only supports positive numbers
-     * @param nibble Character representation of number to convert to binary
-     * @return String representation of binary number using 4 bits
-     */
-    public static String decimal_to_binary(char nibble) {
-        String result_string = "0000"; // case '0'
-        switch (nibble) {
-            case '1':  result_string = "0001"; break;
-            case '2':  result_string = "0010"; break;
-            case '3':  result_string = "0011"; break;
-            case '4':  result_string = "0100"; break;
-            case '5':  result_string = "0101"; break;
-            case '6':  result_string = "0110"; break;
-            case '7':  result_string = "0111"; break;
-            case '8':  result_string = "1000"; break;
-            case '9':  result_string = "1001"; break;
-        }
-        return result_string;
-    }
-    
-    /**
-     * Prepends values lower than 100 by up to two zeroes to make sure number
-     * is in "3-digit number representation" (e.g. 3 -> 003)
-     * @param raw_value number
-     * @return 
-     */
-    public static String prepend_zeroes_if_needed(String raw_value) {
-        if (raw_value.length() == 1) raw_value = "00" + raw_value;
-        else if (raw_value.length() == 2) raw_value = "0" + raw_value;
-        return raw_value;
-    }
-    
+          
     /**
      * Uses Log2SubBand algorithm for decoding.
      * Decodes String of zeroes and ones into comma separated String of numbers
@@ -122,7 +68,6 @@ public class Log2SubBand {
         String current_number = "";
         String[] results;
         String decoded_string = "";
-        
         
         while (!remaining_string.isEmpty()) {
             if (debug) System.out.print("remaining: " + remaining_string + "(" + remaining_string.length() + ")\n");
@@ -163,84 +108,10 @@ public class Log2SubBand {
         return stuff_to_return;
     }
     
-    public static String get_decimal_digit(String binary) {
-        String decoded_digit = "";
-        switch (binary) {
-            case "0000":  decoded_digit = "0"; break;
-            case "0001":  decoded_digit = "1"; break;
-            case "0010":  decoded_digit = "2"; break;
-            case "0011":  decoded_digit = "3"; break;
-            case "0100":  decoded_digit = "4"; break;
-            case "0101":  decoded_digit = "5"; break;
-            case "0110":  decoded_digit = "6"; break;
-            case "0111":  decoded_digit = "7"; break;
-            case "1000":  decoded_digit = "8"; break;
-            case "1001":  decoded_digit = "9"; break;
-        }
-        return decoded_digit;
-    }
-    
     public static Double compression_percentage(String overall_compressed, String overall_uncompressed) {
         return Math.round(1000.0 * overall_compressed.length()/overall_uncompressed.length())/10.0;
     }
     
-    /**
-     * Provides UI element for user to select a file.
-     * @return String absolute path of the file selected by user
-     */
-    public static String request_file() {
-        JButton open_btn = new JButton();
-        JFileChooser fc = new JFileChooser();
-        fc.setCurrentDirectory(new java.io.File("../test files"));
-        fc.setDialogTitle("Please select a file");
-        if (fc.showOpenDialog(open_btn) == JFileChooser.APPROVE_OPTION) return fc.getSelectedFile().getAbsolutePath();
-        else return "";
-    }
-    /**
-     * Attempts to parse a CSV file.
-     * !!! Currently no check if file is a valid csv file
-     * @param file_path Path to a file to parse
-     * @return
-     * @throws java.io.FileNotFoundException
-     */
-    public static String[] parse_CSV (String file_path){
-        CSVReader reader = null;
-        try {reader = new CSVReader(new FileReader(file_path));}
-        catch (FileNotFoundException ex) {Logger.getLogger(Log2SubBand.class.getName()).log(Level.SEVERE, null, ex);} // autogenerated
-        String[] data_parsed = null;
-        if (reader instanceof CSVReader) {
-            String comma_sep_values = "";
-            try {List<String[]> my_data = reader.readAll();
-                try {
-                    for (String[] line : my_data) {
-                        for (String value : line) {
-                            if (value.length() > 0) { // Need to skip empty cells
-                                comma_sep_values += value + ",";
-                            }
-                        }
-                    }
-                    comma_sep_values = comma_sep_values.substring(0, comma_sep_values.length()-1); // remove last comma
-                    data_parsed = comma_sep_values.split(",[ ]*"); // Separates numbers by comma, removes extra spaces
-                    
-                    if(debug) System.out.println(comma_sep_values);
-                } catch (IndexOutOfBoundsException e) {} // No info about size, need to try and read until we get too far         
-            }
-            catch (IOException ex) {Logger.getLogger(Log2SubBand.class.getName()).log(Level.SEVERE, null, ex);}  // autogenerated   
-        }
-        return data_parsed;
-    }
-    
-    /**
-     * Provides a panel with 2 options - manual data entry or importing a csv file
-     * 
-     * @return String selected option 
-     */
-    
-    public static boolean data_entry_option_prompt() {
-        return false;
-    }
-    
-    // @TODO ask user if they want to enter data manually or inport a file instead
     public static void main(String[] args) {
         String overall_compressed = "";
         String overall_uncompressed = "";
@@ -248,25 +119,19 @@ public class Log2SubBand {
         String[] raw_values;
         String input = "";
         
-        String[] options = new String[2];
-        options[0] = "Manual entry";
-        options[1] = "CSV file import";
-        String message = "Would you like to enter data manually or import a csv file with data?";
-        int dialog_result = JOptionPane.showOptionDialog(null,message,"Title", 0,JOptionPane.QUESTION_MESSAGE,null,options,null);
-
-        if(dialog_result == 0) {
-            raw_values = request_input();
+        if(MyUtils.data_entry_option_prompt() == 0) {
+            raw_values = MyUtils.request_input();
         } else {
-            String csv_file = request_file();
-            raw_values = parse_CSV(csv_file);
+            String csv_file = MyUtils.request_file();
+            raw_values = MyUtils.parse_CSV(csv_file);
         }
         
         for (String raw_value : raw_values) {
-            raw_value = prepend_zeroes_if_needed(raw_value);
+            raw_value = MyUtils.prepend_zeroes_if_needed(raw_value);
             input += "," + raw_value;
             
             if (debug) System.out.println("Raw value: " + raw_value);
-            current_compressed = get_compressed_data(raw_value);
+            current_compressed = compressed_data(raw_value);
             if (debug) System.out.println("Current compressed data: " + current_compressed);
             overall_compressed += current_compressed;
             
@@ -282,7 +147,5 @@ public class Log2SubBand {
         double compression_rate = compression_percentage(overall_compressed, overall_uncompressed);
         System.out.println("Overall compression rate: " + compression_rate + "%");
         System.out.println("Decompressed data: " + decode_string(overall_compressed));
-        
-    }
-    
+    }   
 }
---
+++ Log2SubBand/src/log2subband/MyUtils.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package log2subband;
+
+import com.opencsv.CSVReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import static log2subband.Log2SubBand.debug;
+
+/**
+ *
+ * @author Jakub
+ */
+public class MyUtils {
+    
+    /**
+     * Prompts the user to enter an input.
+     * The input numbers are then separated by comma and extra spaces are removed
+     * @return raw_values Array of integers given by user
+     */
+    public static String[] request_input() {
+        String answer = JOptionPane.showInputDialog(null, "Type in numbers separated by comma.");
+        String[] raw_values = answer.split(",[ ]*"); // Separates numbers by comma, removes extra spaces
+        if (debug) System.out.println(answer);
+        return raw_values;
+    }
+    
+    /**
+     * Converts number in character representation(e.g. '1') into binary String representation (e.g. "0001") 
+     * Converted number has 4 digits and only supports positive numbers
+     * @param nibble Character representation of number to convert to binary
+     * @return String representation of binary number using 4 bits
+     */
+    public static String decimal_to_binary(char nibble) {
+        String result_string = "0000"; // case '0'
+        switch (nibble) {
+            case '1':  result_string = "0001"; break;
+            case '2':  result_string = "0010"; break;
+            case '3':  result_string = "0011"; break;
+            case '4':  result_string = "0100"; break;
+            case '5':  result_string = "0101"; break;
+            case '6':  result_string = "0110"; break;
+            case '7':  result_string = "0111"; break;
+            case '8':  result_string = "1000"; break;
+            case '9':  result_string = "1001"; break;
+        }
+        return result_string;
+    }
+    
+    /**
+     * Prepends values lower than 100 by up to two zeroes to make sure number
+     * is in "3-digit number representation" (e.g. 3 -> 003)
+     * @param raw_value number
+     * @return 
+     */
+    public static String prepend_zeroes_if_needed(String raw_value) {
+        if (raw_value.length() == 1) raw_value = "00" + raw_value;
+        else if (raw_value.length() == 2) raw_value = "0" + raw_value;
+        return raw_value;
+    }
+    
+    /**
+     * Takes in binary 4-digit number as a String and returns decimal digit in String type
+     * !!! Works only for numbers inclusively between 0000 and 1001 in binary; or 0 and 9 both in decimal 
+     * @param binary Four digit binary number (e.g. 0011)
+     * @return String decimal number 
+     */
+    public static String get_decimal_digit(String binary) {
+        String decoded_digit = "";
+        switch (binary) {
+            case "0000":  decoded_digit = "0"; break;
+            case "0001":  decoded_digit = "1"; break;
+            case "0010":  decoded_digit = "2"; break;
+            case "0011":  decoded_digit = "3"; break;
+            case "0100":  decoded_digit = "4"; break;
+            case "0101":  decoded_digit = "5"; break;
+            case "0110":  decoded_digit = "6"; break;
+            case "0111":  decoded_digit = "7"; break;
+            case "1000":  decoded_digit = "8"; break;
+            case "1001":  decoded_digit = "9"; break;
+        }
+        return decoded_digit;
+    }
+    
+    /**
+     * Provides UI element for user to select a file.
+     * @return String absolute path of the file selected by user
+     */
+    public static String request_file() {
+        JButton open_btn = new JButton();
+        JFileChooser fc = new JFileChooser();
+        fc.setCurrentDirectory(new java.io.File("../test files"));
+        fc.setDialogTitle("Please select a file");
+        if (fc.showOpenDialog(open_btn) == JFileChooser.APPROVE_OPTION) return fc.getSelectedFile().getAbsolutePath();
+        else return "";
+    }
+    
+    /**
+     * Provides a panel with 2 options - manual data entry or importing a csv file
+     * @return int 0 if user selected manual data entry; 1 when user selected import from a file 
+     */
+    public static int data_entry_option_prompt() {
+        String[] options = new String[2];
+        options[0] = "Manual entry";
+        options[1] = "CSV file import";
+        String message = "Would you like to enter data manually or import a csv file with data?";
+        int dialog_result = JOptionPane.showOptionDialog(null,message,"Title", 0,JOptionPane.QUESTION_MESSAGE,null,options,null);
+        
+        return dialog_result;
+    }
+    
+    /**
+     * Attempts to parse a CSV file.
+     * !!! Currently no check if file is a valid csv file
+     * @param file_path Path to a file to parse
+     * @return String[] of data from a file
+     */
+    public static String[] parse_CSV (String file_path){
+        CSVReader reader = null;
+        try {reader = new CSVReader(new FileReader(file_path));}
+        catch (FileNotFoundException ex) {Logger.getLogger(Log2SubBand.class.getName()).log(Level.SEVERE, null, ex);} // autogenerated
+        String[] data_parsed = null;
+        if (reader instanceof CSVReader) {
+            String comma_sep_values = "";
+            try {List<String[]> my_data = reader.readAll();
+                try {
+                    for (String[] line : my_data) {
+                        for (String value : line) {
+                            if (value.length() > 0) comma_sep_values += value + ",";
+                        }
+                    }
+                    comma_sep_values = comma_sep_values.substring(0, comma_sep_values.length()-1); // remove last comma
+                    data_parsed = comma_sep_values.split(",[ ]*"); // Separates numbers by comma, removes extra spaces
+                    
+                    if(debug) System.out.println(comma_sep_values);
+                } catch (IndexOutOfBoundsException e) {} // No info about size, need to try and read until we get too far         
+            }
+            catch (IOException ex) {Logger.getLogger(Log2SubBand.class.getName()).log(Level.SEVERE, null, ex);}  // autogenerated   
+        }
+        return data_parsed;
+    }
+}
