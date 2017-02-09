383142c3d506207973b88012ad0e1a67be1b233e
jacob135135 <jakubbrezonak@gmail.com>
2017-02-09 14:35:23 +0000

    Added licences

--- Log2SubBand/src/log2subband/HuffmanCode.java
+++ Log2SubBand/src/log2subband/HuffmanCode.java
@@ -1,3 +1,15 @@
+/*  Copyright (c)  2009  Mike Neurohr.
+    Modified by Jakub Brezonak in 2017
+    Permission is granted to copy, distribute and/or modify this document
+    under the terms of the GNU Free Documentation License, Version 1.2
+    or any later version published by the Free Software Foundation;
+    with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+    Texts.  A copy of the license is included in the section entitled "GNU
+    Free Documentation License".
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package log2subband;
 import java.util.*;
